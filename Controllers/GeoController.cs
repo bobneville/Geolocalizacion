0585a26f9ff1c228894f420e3c0eb0d3bdd7ff12
Luis Alberto Fernández Abeleira <alberto.incognita@gmail.com>
2023-05-23 16:25:02 +0200

    Agregar archivos de proyecto.

---
+++ Controllers/GeoController.cs
@@ -0,0 +1,31 @@
+﻿using Geolocalizacion.Modelos;
+using MaxMind.GeoIP2;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Geolocalizacion.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GeoController : ControllerBase
+    {
+        [HttpGet("[action]/{ipAddress}")]
+        public IActionResult GetCountry(string ipAddress)
+        {            
+            using var reader = new DatabaseReader(@"C:\Desarrollo\Geo-Location-API-Net-Core-master\GeoLite2-Country.mmdb");
+
+            var response = reader.Country(ipAddress);
+
+            var geoLocation = new GeoClass
+            {
+                countryName = response.Country.Name,
+                countryIsoCode = response.Country.IsoCode,
+                IsInEuropeanUnion = response.Country.IsInEuropeanUnion
+            };
+
+            return StatusCode(StatusCodes.Status200OK, geoLocation);
+
+
+        }
+    }
+}
---
+++ Modelos/GeoClass.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Geolocalizacion.Modelos
+{
+    public class GeoClass
+    {
+        public string countryName { get; set; }
+        public string countryIsoCode { get; set; }
+        public bool IsInEuropeanUnion { get; set; }
+    }
+}
